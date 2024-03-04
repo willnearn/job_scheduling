89447bfbf6e0ec8ac476934b297b80b1b7172792
Will Nearn <williamnearn@gmail.com>
2024-03-04 13:07:58 -1000

    Modified the combinatorial optimization to fit a week-long schedule with allowed breaks

--- test_google_or_tools.py
+++ test_google_or_tools.py
@@ -12,12 +12,12 @@ def print_preference_sums(preferences):
     rownum = 0
     for row in preferences:
         if abs(sum(row) - 1) > 0.005: #floating point error
-            print("Row "+rownum+"'s sum is "+sum(row)+": "+row)
+            print("Row "+str(rownum)+"'s sum is "+str(sum(row))+": "+str(row))
         rownum += 1
 
 
 def main():
-    preferences = [  #Rows are workers, columns are jobs
+    day_preferences = [  #Rows are workers, columns are jobs
         # KL, HTO, HTC, Field
         [0.6, 0.2, 0.1, 0.1], #Amber
         [0.0, 0.3, 0.3, 0.4], #Nearn
@@ -25,9 +25,30 @@ def main():
         [0.8, 0.1, 0.1, 0.0], #Elinor
         [0.1, 0.5, 0.3, 0.1], #Mariam
     ]
-    print_preference_sums(preferences)
-    num_workers = len(preferences)
-    num_jobs = len(preferences[0])
+    sunday_preferences = [
+        # KL, HTO, HTC, Field
+        [0.0, 0.0, 0.0, 0.0], #Amber
+        [0.0, 0.0, 0.0, 0.0], #Nearn
+        [0.0, 0.0, 0.0, 0.0], #Rob
+        [0.0, 0.0, 0.0, 0.0], #Elinor
+        [0.0, 0.0, 0.0, 0.0], #Mariam
+    ]
+
+    week_preferences = [day_preferences, #EVERYTHING SHOULD BE BASED OFF OF week_preferences FROM HERE ON OUT. NO MENTIONING day_preferences OR sunday_preferences
+                        day_preferences,
+                        day_preferences,
+                        day_preferences,
+                        day_preferences,
+                        day_preferences,
+                        sunday_preferences]
+    
+    for day in week_preferences: #Check for errors
+        print_preference_sums(day)
+    
+    num_days = len(week_preferences)
+    num_workers = len(week_preferences[0])
+    num_jobs = len(week_preferences[0][0])
+    num_days_off_per_week = 3 #CONFIGUREABLE
 
     # Create the mip solver with the SCIP backend.
     solver = pywraplp.Solver.CreateSolver("SCIP") #Not sure what SCIP is
@@ -35,26 +56,36 @@ def main():
     if not solver:
         return
     
-    # assignment_matrix[i, j] is an array of 0-1 variables, which will be 1
-    # if worker i is assigned to task j.
+    # assignment_matrix[d, w, j] is an array of 0-1 variables, which will be 1
+    # if worker w is assigned to task j on day d.
     assignment_matrix = {}
-    for w in range(num_workers):
-        for j in range(num_jobs):
-            assignment_matrix[w, j] = solver.IntVar(0, 1, "")
+    for d in range(num_days):
+        for w in range(num_workers):
+            for j in range(num_jobs):
+                assignment_matrix[d, w, j] = solver.IntVar(0, 1, "") #Initialization
     
-    # Each worker is assigned to at most 1 task.
-    for w in range(num_workers):
-        solver.Add(solver.Sum([assignment_matrix[w, j] for j in range(num_jobs)]) <= 1)
+    # Constraint: Each worker is assigned to at most 1 task per day.
+    for d in range(num_days):
+        for w in range(num_workers):
+            solver.Add(solver.Sum([assignment_matrix[d, w, j] for j in range(num_jobs)]) <= 1)
 
-    # Each task is assigned to at most one worker.
-    for j in range(num_jobs):
-        solver.Add(solver.Sum([assignment_matrix[w, j] for w in range(num_workers)]) <= 1)
-    
-    # Objective function. TODO: Change this to reflect my own objective function
-    objective_terms = []
-    for i in range(num_workers):
+    # Constraint: Each task is assigned to at most one worker per day.
+    for d in range(num_days):
         for j in range(num_jobs):
-            objective_terms.append(preferences[i][j] * assignment_matrix[i, j])
+            sol = [assignment_matrix[d, w, j] for w in range(num_workers)]
+            print(sol)
+            solver.Add(solver.Sum([assignment_matrix[d, w, j] for w in range(num_workers)]) <= 1)
+    
+    # Constraint: Each worker only works for num_days - num_days_off_per_week
+    for w in range(num_workers):
+        solver.Add(solver.Sum([assignment_matrix[d, w, j] for d in range(num_days) for j in range(num_jobs)]) <= (num_days - num_days_off_per_week))
+
+    # Objective function -- a combination of the (double)preference matrix and the (<ortools.linear_solver.pywraplp.Variable>)assignment matrix
+    objective_terms = []
+    for d in range(num_days):
+        for w in range(num_workers):
+            for j in range(num_jobs):
+                objective_terms.append(week_preferences[d][w][j] * assignment_matrix[d, w, j])
     solver.Maximize(solver.Sum(objective_terms))
     
     #Solve
@@ -67,11 +98,17 @@ def main():
         if status == pywraplp.Solver.FEASIBLE:
             print("Good solution found.")
         print(f"Total Happiness = {solver.Objective().Value()}\n")
-        for i in range(num_workers):
-            for j in range(num_jobs):
-                # Test if assignment_matrix[i,j] is 1 (with tolerance for floating point arithmetic).
-                if assignment_matrix[i, j].solution_value() > 0.5:
-                    print(f"Worker {i} assigned to task {j}." + f" Happiness: {preferences[i][j]}")
+        for d in range(num_days):
+            print(f"\n\t---- Day {d} ----")
+            for w in range(num_workers):
+                assigned = False
+                for j in range(num_jobs):
+                    # Test if assignment_matrix[d,w,j] is 1 (with tolerance for floating point arithmetic).
+                    if assignment_matrix[d, w, j].solution_value() > 0.5:
+                        print(f"Worker {w} assigned to task {j}." + f" Happiness: {week_preferences[d][w][j]}")
+                        assigned = True
+                if not assigned:
+                    print(f"Worker {w} unassigned.")
     else:
         print("No solution found.")
 
