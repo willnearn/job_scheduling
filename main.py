9fb61fcdf944440c4943df2c147485ca9591fd66
Will Nearn <williamnearn@gmail.com>
2024-03-04 15:29:43 -1000

    File now reads in input from .xlsx documents and multiplies them together to get a composite "happiness" score

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Assignment Problem Example
 based off of example in https://developers.google.com/optimization/assignment/assignment_example, but modified for my understanding
 """
 import pandas as pd
+import numpy as np
 from ortools.linear_solver import pywraplp
 
 def print_preference_sums(preferences):
@@ -17,9 +18,18 @@ def print_preference_sums(preferences):
 
 
 def main():
-    worker_names = ["Amber", "Nearn", "Rob", "Elinor", "Mariam"] #Need to pair this with the input data and extract it
-    job_names = ["Konalani", "Hilltop Opener", "Hilltop Closer", "Field"] #Best to pair this with the input data and extract it
-    day_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] #I'm fine with this one staying hard-coded
+    worker_xlsx = pd.read_excel('worker_preferences.xlsx', sheet_name=None, index_col=0)
+    manager_xlsx = pd.read_excel('manager_preferences.xlsx', sheet_name=None, index_col=0)
+    worker_names = list(worker_xlsx.keys()) # MAKE SURE THAT THE NAMES ARE THE SAME ON manager_preferences.xlsx AS worker_preferences.xlsx !!!
+    job_names = list(worker_xlsx[worker_names[0]].index)  # MAKE SURE THAT THE SAME JOB NAMES EXIST ON ALL SHEETS OF manager_preferences.xlsx AND worker_preferences.xlsx !!!
+    day_names = list(worker_xlsx[worker_names[0]].head()) # MAKE SURE THAT THE SAME DAY NAMES EXIST ON ALL SHEETS OF manager_preferences.xlsx AND worker_preferences.xlsx !!!
+    week_preferences = []
+    for index in range(len(worker_names)):
+        week_preferences.append(np.multiply(
+            worker_xlsx[worker_names[index]].to_numpy(),
+            manager_xlsx[worker_names[index]].to_numpy()
+        ))
+    """
     day_preferences = [  #Rows are workers, columns are jobs
         # KL, HTO, HTC, Field
         [0.6, 0.2, 0.1, 0.1], #Amber
@@ -46,11 +56,11 @@ def main():
                         sunday_preferences]
     
     for day in week_preferences: #Check for errors
-        print_preference_sums(day)
+        print_preference_sums(day)"""
     
-    num_days = len(week_preferences)
-    num_workers = len(week_preferences[0])
-    num_jobs = len(week_preferences[0][0])
+    num_workers = len(week_preferences)
+    num_jobs = len(week_preferences[0])
+    num_days = len(week_preferences[0][0])
     num_days_off_per_week = 3 #CONFIGUREABLE
 
     # Create the mip solver with the SCIP backend.
@@ -62,33 +72,33 @@ def main():
     # assignment_matrix[d, w, j] is an array of 0-1 variables, which will be 1
     # if worker w is assigned to task j on day d.
     assignment_matrix = {}
-    for d in range(num_days):
-        for w in range(num_workers):
-            for j in range(num_jobs):
-                assignment_matrix[d, w, j] = solver.IntVar(0, 1, "") #Initialization
+    for w in range(num_workers): 
+        for j in range(num_jobs):
+            for d in range(num_days):
+                assignment_matrix[w, j, d] = solver.IntVar(0, 1, "") #Initialization
     
     # Constraint: Each worker is assigned to at most 1 task per day.
     for d in range(num_days):
         for w in range(num_workers):
-            solver.Add(solver.Sum([assignment_matrix[d, w, j] for j in range(num_jobs)]) <= 1)
+            solver.Add(solver.Sum([assignment_matrix[w, j, d] for j in range(num_jobs)]) <= 1)
 
     # Constraint: Each task is assigned to at most one worker per day.
     for d in range(num_days):
         for j in range(num_jobs):
-            sol = [assignment_matrix[d, w, j] for w in range(num_workers)]
+            sol = [assignment_matrix[w, j, d] for w in range(num_workers)]
             print(sol)
-            solver.Add(solver.Sum([assignment_matrix[d, w, j] for w in range(num_workers)]) <= 1)
+            solver.Add(solver.Sum([assignment_matrix[w, j, d] for w in range(num_workers)]) <= 1)
     
     # Constraint: Each worker only works for num_days - num_days_off_per_week
     for w in range(num_workers):
-        solver.Add(solver.Sum([assignment_matrix[d, w, j] for d in range(num_days) for j in range(num_jobs)]) <= (num_days - num_days_off_per_week))
+        solver.Add(solver.Sum([assignment_matrix[w, j, d] for d in range(num_days) for j in range(num_jobs)]) <= (num_days - num_days_off_per_week))
 
     # Objective function -- a combination of the (double)preference matrix and the (<ortools.linear_solver.pywraplp.Variable>)assignment matrix
     objective_terms = []
     for d in range(num_days):
         for w in range(num_workers):
             for j in range(num_jobs):
-                objective_terms.append(week_preferences[d][w][j] * assignment_matrix[d, w, j])
+                objective_terms.append(week_preferences[w][j][d] * assignment_matrix[w, j, d])
     solver.Maximize(solver.Sum(objective_terms))
     
     #Solve
@@ -103,18 +113,18 @@ def main():
         print(f"Total Happiness = {solver.Objective().Value()}\n")
         data = {} #For the output DataFrame
         for d in range(num_days):
-            print(f"\n\t---- Day {d} ----")
+            print(f"\n\t---- {day_names[d]} ----")
             day_assignments = {day_names[d] : ["NOBODY" for _ in range(num_jobs)]}
             for w in range(num_workers):
                 assigned = False
                 for j in range(num_jobs):
-                    # Test if assignment_matrix[d,w,j] is 1 (with tolerance for floating point arithmetic).
-                    if assignment_matrix[d, w, j].solution_value() > 0.5:
-                        print(f"Worker {w} assigned to task {j}." + f" Happiness: {week_preferences[d][w][j]}")
+                    # Test if assignment_matrix[w, j, d] is 1 (with tolerance for floating point arithmetic).
+                    if assignment_matrix[w, j, d].solution_value() > 0.5:
+                        print(f"{worker_names[w]} is assigned to {job_names[j]}." + f" Happiness: {week_preferences[w][j][d]}")
                         day_assignments[day_names[d]][j] = worker_names[w]
                         assigned = True
                 if not assigned:
-                    print(f"Worker {w} unassigned.")
+                    print(f"{worker_names[w]} is off.")
             data[day_names[d]] = day_assignments[day_names[d]]
         df = pd.DataFrame(data, index=job_names)
         df.to_csv("output.csv", index=True)
